d63833b95c7eab164219c7fe76108017af486088
Emma Alexandria <emma.jellemabutler@gmail.com>
2024-12-25 11:17:49 +0200

    Delete old dir

--- packages/themewind/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { InternalConfig, tailthemes, TailwindTheme } from "./plugin";
-
-
-export type TailthemesConfig = InternalConfig<TailwindTheme>
-export { tailthemes }
-
--- packages/themewind/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup"
-
-export default defineConfig({
-	entry: ["./src/index.ts", "./src/themes/*"],
-	dts: true,
-	sourcemap: true,
-	format: ["cjs", "esm"],
-	clean: true,
-})
